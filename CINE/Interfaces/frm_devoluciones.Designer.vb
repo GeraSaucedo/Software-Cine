2f9c6dd6ce9c739f94b48e8c97c9f0d18a9a463d
Gerardo saucedo <JEGSG@hotmail.es>
2018-03-10 15:38:05 -0600

    Formulario devoluciones empezado

---
+++ CINE/Interfaces/frm_devoluciones.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_devoluciones
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(31, 244)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(141, 13)
+        Me.Label8.TabIndex = 118
+        Me.Label8.Text = "Numero de piezas devueltas"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(178, 241)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(104, 20)
+        Me.TextBox3.TabIndex = 117
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(196, 279)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(86, 23)
+        Me.Button6.TabIndex = 113
+        Me.Button6.Text = "Salir"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(120, 279)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(70, 23)
+        Me.Button4.TabIndex = 111
+        Me.Button4.Text = "Cancelar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(34, 279)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(80, 23)
+        Me.Button2.TabIndex = 109
+        Me.Button2.Text = "Guardar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(201, 214)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox5.TabIndex = 108
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(145, 214)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox4.TabIndex = 107
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(88, 214)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox3.TabIndex = 106
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(34, 217)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 101
+        Me.Label5.Text = "Fecha"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(28, 57)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(250, 68)
+        Me.RichTextBox2.TabIndex = 100
+        Me.RichTextBox2.Text = ""
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 41)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(109, 13)
+        Me.Label4.TabIndex = 99
+        Me.Label4.Text = "Motivo de devolucion"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(28, 144)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(250, 21)
+        Me.ComboBox2.TabIndex = 98
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(25, 128)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 97
+        Me.Label3.Text = "Empleado"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(51, 12)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(135, 20)
+        Me.TextBox8.TabIndex = 92
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(25, 15)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(18, 13)
+        Me.Label14.TabIndex = 91
+        Me.Label14.Text = "ID"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(192, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(86, 23)
+        Me.Button1.TabIndex = 119
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(28, 187)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(250, 21)
+        Me.ComboBox1.TabIndex = 121
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 171)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 120
+        Me.Label1.Text = "Proveedor"
+        '
+        'frm_devoluciones
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(309, 323)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label14)
+        Me.Name = "frm_devoluciones"
+        Me.Text = "frm_devoluciones"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+End Class
--- CINE/Interfaces/frm_productos.Designer.vb
+++ CINE/Interfaces/frm_productos.Designer.vb
@@ -46,6 +46,10 @@ Partial Class frm_productos
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'TextBox8
@@ -199,7 +203,7 @@ Partial Class frm_productos
         '
         'Button6
         '
-        Me.Button6.Location = New System.Drawing.Point(374, 259)
+        Me.Button6.Location = New System.Drawing.Point(381, 263)
         Me.Button6.Name = "Button6"
         Me.Button6.Size = New System.Drawing.Size(163, 23)
         Me.Button6.TabIndex = 85
@@ -208,7 +212,7 @@ Partial Class frm_productos
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(20, 259)
+        Me.Button5.Location = New System.Drawing.Point(27, 263)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(175, 23)
         Me.Button5.TabIndex = 84
@@ -217,7 +221,7 @@ Partial Class frm_productos
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(374, 227)
+        Me.Button4.Location = New System.Drawing.Point(381, 231)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(163, 23)
         Me.Button4.TabIndex = 83
@@ -226,7 +230,7 @@ Partial Class frm_productos
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(203, 227)
+        Me.Button3.Location = New System.Drawing.Point(210, 231)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(165, 23)
         Me.Button3.TabIndex = 82
@@ -235,7 +239,7 @@ Partial Class frm_productos
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(22, 227)
+        Me.Button2.Location = New System.Drawing.Point(29, 231)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(175, 23)
         Me.Button2.TabIndex = 81
@@ -251,11 +255,47 @@ Partial Class frm_productos
         Me.Button1.Text = "Buscar"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(425, 196)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(55, 13)
+        Me.Label8.TabIndex = 90
+        Me.Label8.Text = "Devueltos"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(481, 193)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(65, 20)
+        Me.TextBox3.TabIndex = 89
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(297, 196)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 13)
+        Me.Label9.TabIndex = 88
+        Me.Label9.Text = "En stock"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(352, 193)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(67, 20)
+        Me.TextBox4.TabIndex = 87
+        '
         'frm_productos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(562, 295)
+        Me.ClientSize = New System.Drawing.Size(562, 293)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
@@ -311,4 +351,8 @@ Partial Class frm_productos
     Friend WithEvents Button3 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox4 As TextBox
 End Class
